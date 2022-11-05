227b903ec84b4f9ca5714577ea343cdbbb996299
critoma <cristianvtoma@gmail.com>
2022-11-05 12:29:03 +0200

    Update Prog08_FcnLambdaDestructDecl.kt

--- JavaKotlinAndroidApps/s14_kotlin/src/eu/ase/ktest/Prog08_FcnLambdaDestructDecl.kt
+++ JavaKotlinAndroidApps/s14_kotlin/src/eu/ase/ktest/Prog08_FcnLambdaDestructDecl.kt
@@ -1,3 +1,46 @@
+
+package eu.dice.ism.ktest
+//data class Student(val a: String, val b: String) {
+class Student(val a: String, val b: String) {
+    var name: String = a
+    var id: String = b
+    
+    fun printStudentWLamda(action: (String) -> Unit ) {
+    	action(this.id + "," + this.name)
+	}
+}
+fun MyFun(x: String):String {
+    var c:String = "Hey! "
+    return (c + x)
+}
+fun MyFunFP(a: String, action: (String) -> Unit ) {
+    print("\n Hello Functional Programming - ")
+    action(a)
+}
+
+fun main(args: Array<String>) {
+    // lambda expression:
+    val myLambda: (s:String) -> Unit = {
+        s:String -> println(s)
+		//var sr:String = s; sr += "..."; println(sr)
+    }
+    // 1. std fun call
+    val fresult = MyFun("Hello from std function")
+    println(fresult)
+   
+    
+    // 2. Lambda call simple:
+    myLambda("Hello Lambda from Kotlin")
+    
+    // 3. Lambda call Functional Programming:
+    MyFunFP("Playing with Kotlin ", myLambda)
+    
+    // 4. OOP + FP
+    val s = Student("John", "5010721cnp")
+    s.printStudentWLamda(myLambda)
+}
+
+/*
 package eu.ase.ktest
 
 // destructuring declarations:
@@ -36,3 +79,4 @@ fun myFun(a: String, action: (String)->Unit) { //passing lambda
    print("Heyyy!!!")
    action(a) // call to lambda function
 }
+*/
